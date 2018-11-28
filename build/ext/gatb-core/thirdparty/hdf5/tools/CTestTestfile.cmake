a015f227bb38e360f79409752b1bb1beba1caa4e
fabrice legeai <flegeai@irisa.fr>
2018-11-28 09:39:53 +0100

    removed the build directory

--- build/ext/gatb-core/thirdparty/hdf5/tools/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/flegeai/workspace/Con10x/thirdparty/gatb-core/thirdparty/hdf5/tools
-# Build directory: /home/flegeai/workspace/Con10x/build/ext/gatb-core/thirdparty/hdf5/tools
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(lib)
-subdirs(h5dump)
--- build/ext/gatb-core/thirdparty/hdf5/tools/h5dump/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/flegeai/workspace/Con10x/thirdparty/gatb-core/thirdparty/hdf5/tools/h5dump
-# Build directory: /home/flegeai/workspace/Con10x/build/ext/gatb-core/thirdparty/hdf5/tools/h5dump
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
