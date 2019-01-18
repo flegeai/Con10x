b7b8bbae2b9c3e2dd9d63587de2ff5576fe07f59
fabrice legeai <flegeai@irisa.fr>
2019-01-18 11:53:27 +0100

    New version with code factorization and simple idnexation

--- tools/GetReads/src/GetReads.cpp
+++ tools/GetReads/src/GetReads.cpp
@@ -1,11 +1,10 @@
 #include <gatb/gatb_core.hpp>
 #include <iostream>
 #include <fstream>
-#include "../../Con10x/Fragment10x.hpp"
+#include <IndexReads_Hashed.hpp>
+#include <IndexReads_Interleaved.hpp>
 #include <string>
-#include <boost/serialization/map.hpp>
-#include <boost/serialization/list.hpp>
-#include <boost/archive/binary_iarchive.hpp>
+
 
 /********************************************************************************/
 /*                                                                              */
@@ -18,43 +17,46 @@ int main (int argc, char* argv[])
 
     static const char* READS = "-reads";
     static const char* BARCODE = "-barcodes";
+    static const char* INDEX = "-index";
 
     OptionsParser parser ("getReads");
 
-    parser.push_back (new OptionOneParam (READS, "file of indexed reads",   true));
+    parser.push_back (new OptionOneParam (READS, "file of indexed reads",   false));
     parser.push_back (new OptionOneParam (BARCODE, "barcodes",   true));
+    parser.push_back (new OptionOneParam (INDEX, "barcodes index file",   true));
+
     try
     {
         IProperties* options = parser.parse (argc, argv);
-        string reads_file = options->getStr(READS);
+
         string barcode_file = options->getStr(BARCODE);
+        string index_file = options->getStr(INDEX);
 
-        std::ifstream barcodemap_index(reads_file + ".idx", std::ios::binary);
-        std::map<std::string,std::list<long long>>  barcode_map;
-        boost::archive::binary_iarchive iarch(barcodemap_index);
-        iarch & barcode_map;
-
-        ifstream  barcodemap_file_r (reads_file);
-
-         ifstream barcodes (barcode_file);
-         if (barcodes.is_open()){
-            string barcode;
-            while ( getline (barcodes,barcode) )
-            {
-                std::list<long long>::iterator it;
-                for (it = barcode_map[barcode].begin(); it != barcode_map[barcode].end(); ++it) {
-                  barcodemap_file_r.seekg((streampos) *it);
-                  for (int i =0; i<=7; i++) {
-                    string pair;
-                    std::getline(barcodemap_file_r,pair);
-                    cout << pair<< endl;
-                  }
-                }
-            }
-            barcodes.close();
+        if ( parser.saw(READS)) {
+          std::string itl_file = options->getStr(READS);
+          IndexReads_Interleaved idRI(itl_file, "rd");
+          idRI.load(index_file);
+          ifstream barcodes (barcode_file);
+          if (barcodes.is_open()){
+              string barcode;
+              while ( getline (barcodes,barcode) ){idRI.get(barcode);}
           }
+          barcodes.close();
+        }
+        else {
+          IndexReads_Hashed idRH;
+          idRH.load(index_file);
+          ifstream barcodes (barcode_file);
+           if (barcodes.is_open()){
+               string barcode;
+               while ( getline (barcodes,barcode) ){
+                 cout << barcode << endl;
+                 idRH.get(barcode);
+               }
+           }
+          barcodes.close();
+        }
 
-          else cout << "Unable to open " + barcode_file << endl;
       }
     catch (OptionFailure& e)
     {
--- tools/IndexReads/src/IndexReads.cpp
+++ tools/IndexReads/src/IndexReads.cpp
@@ -1,78 +1,70 @@
 #include <gatb/gatb_core.hpp>
-#include <iostream>
-#include <fstream>
-#include "../../Con10x/Fragment10x.hpp"
+#include "Fragment10x.hpp"
+#include "IndexReads_Hashed.hpp"
+#include "IndexReads_Interleaved.hpp"
 #include <string>
-#include <boost/serialization/map.hpp>
-#include <boost/serialization/list.hpp>
-#include <boost/archive/binary_oarchive.hpp>
 
 /********************************************************************************/
 /*                                                                              */
 /*             IndexReads.cpp                                                   */
 /* this is a simple test to rapidly index 10x reads using their barcodes         */
 /********************************************************************************/
+using namespace std;
 
 int main (int argc, char* argv[])
 {
 
     static const char* STR_BANK1 = "-one";
     static const char* STR_BANK2 = "-two";
-    static const char* OUT_BANK = "-out";
+    static const char* ITL_BANK = "-itl";
 
-    OptionsParser parser ("TwoBanks");
+    OptionsParser parser ("Index");
 
-    parser.push_back (new OptionOneParam (STR_BANK1, "bank one",   true));
-    parser.push_back (new OptionOneParam (STR_BANK2, "bank two",   true));
-    parser.push_back (new OptionOneParam (OUT_BANK, "output bank (interleaved)",   true));
+    parser.push_back (new OptionOneParam (STR_BANK1, "bank one", true));
+    parser.push_back (new OptionOneParam (STR_BANK2, "bank two", true));
+    parser.push_back (new OptionOneParam (ITL_BANK, "interleaved bank", false));
     try
     {
         IProperties* options = parser.parse (argc, argv);
         // We open the two banks
-        IBank* bank1 = Bank::open (options->getStr(STR_BANK1));  LOCAL (bank1);
-        IBank* bank2 = Bank::open (options->getStr(STR_BANK2));  LOCAL (bank2);
-        ofstream output_bank (options->getStr(OUT_BANK));
+        IBank* bank1 = Bank::open (options->getStr(STR_BANK1));
+        LOCAL (bank1);
+        IBank* bank2 = Bank::open (options->getStr(STR_BANK2));
+        LOCAL (bank2);
 
         // We iterate the two banks. Note how we provide two iterators from the two banks.
         PairedIterator<Sequence> *  itPair  = new  PairedIterator<Sequence> (bank1->iterator(), bank2->iterator());
         LOCAL(itPair);
         ProgressIterator< std::pair <Sequence, Sequence>> progress_iter (itPair, "", bank1->estimateNbItems());
 
-        /* Map creation */
-        //std::map<std::string, Fragment_map>> barcode_map;
-        //std::map<std::string,std::list<streampos>> barcode_map;
-        std::map<std::string,std::list<long long>> barcode_map;
+        //IndexReads_Interleaved idRI(options->getStr(ITL_BANK));
+      //  if (!options->getStr(ITL_BANK).empty()) {
+      if ( parser.saw(ITL_BANK)) {
+          std::string itl_file = options->getStr(ITL_BANK);
+          IndexReads_Interleaved idRI(itl_file, "app");
 
-        for (progress_iter.first(); !progress_iter.isDone(); progress_iter.next())
-        {
-            Sequence& s1 = itPair->item().first;
-            Sequence& s2 = itPair->item().second;
-            //std::cout << " avant : " << s1.toString() << endl;
-            Fragment10x f(s1,s2);
+          for (progress_iter.first(); !progress_iter.isDone(); progress_iter.next()){
+              Sequence& s1 = itPair->item().first;
+              Sequence& s2 = itPair->item().second;
+              Fragment10x frag(s1,s2);
+              idRI.add(frag);
+            }
 
-            barcode_map[f.barcode].push_back((long long) output_bank.tellp());
-            output_bank << '@'+f.r1.getComment()  << endl;
-            output_bank << f.r1.toString()  << endl;
-            output_bank << '+'  << endl;
-            output_bank << f.r1.getQuality() << endl;
-            output_bank << '@'+f.r2.getComment()  << endl;
-            output_bank << f.r2.toString()  << endl;
-            output_bank << '+'  << endl;
-            output_bank << f.r2.getQuality() << endl;
+            idRI.save(itl_file+".idx");
         }
+        else {
+            IndexReads_Hashed idRH;
 
-        // register the map with boost
-        std::string index = options->getStr(OUT_BANK);
-        ofstream barcodemap_index (index+".idx");
-        boost::archive::binary_oarchive oarch(barcodemap_index);
-        oarch << barcode_map;
-        output_bank.close();
-        barcodemap_index.close();
+            for (progress_iter.first(); !progress_iter.isDone(); progress_iter.next()){
+                Sequence& s1 = itPair->item().first;
+                Sequence& s2 = itPair->item().second;
+                Fragment10x frag(s1,s2);
+                idRH.add(frag);
+            }
+            
+            idRH.save("barcode.idx");
+        }
       }
-    catch (OptionFailure& e)
-    {
-        return e.displayErrors (std::cout);
-    }
     catch (Exception& e)
     {
         std::cerr << "EXCEPTION: " << e.getMessage() << std::endl;
